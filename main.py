d568d212f2243f93f814040135b6fbdb5f61e1dd
Алексей <Alexey.t684@yandex.ru>
2022-08-31 22:36:38 +0300

    up

--- main.py
+++ main.py
@@ -5,16 +5,16 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 
-s = Service('chromedriver.exe')
-driver = webdriver.Chrome(service=s)
-
 
 def send_message(phone_number, text, count):
+    s = Service('chromedriver.exe')
+    driver = webdriver.Chrome(service=s)
+
     try:
         driver.get(url='https://web.whatsapp.com/')
         sleep(60)
         driver.get(url=f'https://web.whatsapp.com/send?phone={phone_number}')
-        WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH,
+        WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH,
                                                                           '//*[@id="main"]/footer/div[1]/div/span['
                                                                           '2]/div/div[2]/div[1]/div/div[1]')))
 
@@ -24,7 +24,7 @@ def send_message(phone_number, text, count):
         for _ in range(count):
             text_box.send_keys(text)
             text_box.send_keys('\n')
-        # 89671086876
+
         print(f'Сообщение отправлено {count} раз')
         sleep(5)
 
@@ -35,7 +35,7 @@ def send_message(phone_number, text, count):
         driver.close()
         driver.quit()
 
-    return 'Атака завершена'
+    return
 
 
 def main():
